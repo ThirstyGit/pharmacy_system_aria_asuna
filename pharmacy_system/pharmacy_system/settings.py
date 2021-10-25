239667abf92a658cd3b04956ef4122b0eb5da0da
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-10-26 01:03:01 +0600

    Created Initial Register page

--- pharmacy_system/pharmacy_system/settings.py
+++ pharmacy_system/pharmacy_system/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pharmacy.apps.PharmacyConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
