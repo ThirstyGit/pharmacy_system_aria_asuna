8b8f9bbd41c307d457fbfbac0ec2b9fd56659dbb
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-10-31 02:33:30 +0600

    Page hrefs are now properly working

--- pharmacy_system/home/urls.py
+++ pharmacy_system/home/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about', views.about, name='about'),
-  path('cart', views.cart, name='cart'),
-  path('contact', views.contact, name='contact'),
-  path('product', views.product, name='product'),
+  path('about/', views.about, name='about'),
+  path('cart/', views.cart, name='cart'),
+  path('contact/', views.contact, name='contact'),
+  path('product/', views.product, name='product'),
 ]
