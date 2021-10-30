1c75cde7abb146f2b8a9e9b8ba9ace9483f21f08
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-10-31 02:09:45 +0600

    Added paths for the pages from aria's part

--- pharmacy_system/home/views.py
+++ pharmacy_system/home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home/home.html')
+
+def about(request):
+  return render(request, 'home/about.html')
+
+def cart(request):
+  return render(request, 'home/cart.html')
+
+def product(request):
+  return render(request, 'home/product.html')
+
+def contact(request):
+  return render(request, 'home/contact.html')
